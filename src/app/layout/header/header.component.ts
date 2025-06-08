3a57defe94e27897caeba83f185a9d8f2fae9683
dominhdang148 <myclone235@gmail.com>
2025-06-08 23:37:43 +0700

    #3 - finished left part

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {
+  ChevronDown,
+  ComponentIcon,
+  FolderOpenDot,
+  LucideAngularModule,
+} from 'lucide-angular';
+import { TooltipComponent } from '../../shared/components/tooltip/tooltip.component';
+import { PopoverComponent } from './popover/popover.component';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    LucideAngularModule,
+    TooltipComponent,
+    PopoverComponent,
+    RouterLink,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  readonly Logo = ComponentIcon;
+  readonly Project = FolderOpenDot;
+  readonly ChevronDown = ChevronDown;
+}
